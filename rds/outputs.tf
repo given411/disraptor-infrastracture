1d388864f47f4c53f55b29806b46c9089b36f368
given makofane <given.makofane@disraptor.co.za>
2023-08-14 11:25:45 +0200

    rds

--- rds/outputs.tf
+++ rds/outputs.tf
@@ -1,4 +1,6 @@
-output "db_instance_endpoint" {
-  value       = aws_db_instance.myinstance.endpoint
+output "rds_instance_id" {
+  description = "ID of the created RDS instance"
+  value       = aws_db_instance.rds_instance.id
 }
+
 # to add more for dynamic use-cases
