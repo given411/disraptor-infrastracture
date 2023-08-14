9376e729b3734d1b50b6ddf02311116e8338fcf1
Given Makofane <134513212+given411@users.noreply.github.com>
2023-08-14 13:05:54 +0200

    Update main.tf

--- rds/main.tf
+++ rds/main.tf
@@ -1,17 +1,19 @@
-
 resource "aws_db_instance" "rds_instance" {
- 
-  allocated_storage    = 20
-  storage_type        = "gp2"
-  engine              = "mysql"
-  engine_version      = "5.7"
-  instance_class      = "db.t2.micro"
-  name                = var.db_instance_identifier
-  username            = "admin"
-  password            = "MySecureP@ssw0rd"  # Update with a valid password
-  parameter_group_name = "default.mysql5.7"
+  allocated_storage = 20
+  identifier        = "rds-terraform"
+  storage_type      = "gp2"
+  engine            = "mysql"
+  //engine_version = "8.0.27"
+  instance_class = "db.t2.micro"
+  name           = var.db_name
+  username       = "admin"
+  password       = var.db_password
+
+  publicly_accessible = true
+  skip_final_snapshot = true
+
+
   tags = {
-    Name = "lms-test-rds"
+    Name = "ls-test"
   }
 }
-# to add more for dynamic use-cases
