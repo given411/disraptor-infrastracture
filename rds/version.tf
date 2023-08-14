b63f4b4d50e26a7ea5bffeec427e71b4190d19a5
Given Makofane <134513212+given411@users.noreply.github.com>
2023-08-14 13:08:03 +0200

    Update version.tf

--- rds/version.tf
+++ rds/version.tf
@@ -1,11 +1,10 @@
 terraform {
-  required_version = ">= 1.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 3.27"
     }
   }
- //backend "s3" {}
+
+  required_version = ">= 0.14.9"
 }
