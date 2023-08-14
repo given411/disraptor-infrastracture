d84d513621f00c2bfdf34a082e6d7db4dc7eda26
Given Makofane <134513212+given411@users.noreply.github.com>
2023-08-14 09:44:16 +0200

    Update version.tf

--- vpc/version.tf
+++ vpc/version.tf
@@ -1,14 +1,11 @@
 terraform {
-required_version = "1.4.0
+  required_version = ">= 1.0"
 
   required_providers {
     aws = {
-
       source  = "hashicorp/aws"
-
-      version = ">= 2.70.0"
-
+      version = ">= 5.0"
     }
-}
+  }
  backend "s3" {}
 }
