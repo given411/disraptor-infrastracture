d830c3e7005a5d917d976f2856de8ed3782daea7
given makofane <given.makofane@disraptor.co.za>
2023-08-18 11:16:47 +0200

    vpc-update

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -580,29 +580,6 @@ output "default_vpc_main_route_table_id" {
   value       = try(aws_default_vpc.this[0].main_route_table_id, null)
 }
 
-################################################################################
-# VPC Flow Log
-################################################################################
-
-output "vpc_flow_log_id" {
-  description = "The ID of the Flow Log resource"
-  value       = try(aws_flow_log.this[0].id, null)
-}
-
-output "vpc_flow_log_destination_arn" {
-  description = "The ARN of the destination for VPC Flow Logs"
-  value       = local.flow_log_destination_arn
-}
-
-output "vpc_flow_log_destination_type" {
-  description = "The type of the destination for VPC Flow Logs"
-  value       = var.flow_log_destination_type
-}
-
-output "vpc_flow_log_cloudwatch_iam_role_arn" {
-  description = "The ARN of the IAM role used when pushing logs to Cloudwatch log group"
-  value       = local.flow_log_iam_role_arn
-}
 
 ################################################################################
 # Static values (arguments)
